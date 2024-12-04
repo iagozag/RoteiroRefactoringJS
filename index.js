bb17bdd56e175b8a18e405eba3d0c14864abf382
iagozag <iagozag@gmail.com>
2024-12-03 22:51:18 -0300

    Commit 2 - Replace Temp with Query

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ function gerarFaturaStr(fatura, pecas) {
                           { style: "currency", currency: "BRL",
                             minimumFractionDigits: 2 }).format;
 
-    function calcularTotalApresentacao(apre, peca) {
+    function getPeca(apresentacao) {
+        return pecas[apresentacao.id];
+    }
+
+    function calcularTotalApresentacao(apre) {
+        const peca = getPeca(apre);
         let total = 0;
         switch (peca.tipo) {
             case "tragedia":
@@ -31,17 +36,16 @@ function gerarFaturaStr(fatura, pecas) {
     }
 
     for (let apre of fatura.apresentacoes) {
-        const peca = pecas[apre.id];
-        let total = calcularTotalApresentacao(apre, peca);
+        let total = calcularTotalApresentacao(apre);
 
         // Créditos para próximas contratações
         creditos += Math.max(apre.audiencia - 30, 0);
-        if (peca.tipo === "comedia") {
+        if (getPeca(apre).tipo === "comedia") {
             creditos += Math.floor(apre.audiencia / 5);
         }
 
         // Mais uma linha da fatura
-        faturaStr += `  ${peca.nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
+        faturaStr += `  ${getPeca(apre).nome}: ${formato(total / 100)} (${apre.audiencia} assentos)\n`;
         totalFatura += total;
     }
     faturaStr += `Valor total: ${formato(totalFatura / 100)}\n`;
